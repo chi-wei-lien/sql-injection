d21a6fa49d263ac838f4bf27b60549a04ca7efbf
chi-wei-lien <lien2@purdue.edu>
2022-03-11 21:23:00 -0500

    set up db

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
 var mysql = require('mysql');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "yourpassword"
+    password: process.env.DB_PASS,
+    database: "mydb"
   });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("MySQL Connected!");
+});
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
